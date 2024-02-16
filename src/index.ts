7f43a318054f7ee0876b67d2dd8ca172abef4300
Ben Ballard <benb971@gmail.com>
2024-02-16 12:14:22 -0600

    eco config update

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,8 @@ const app = express()
 const whitelist = [
   'http://localhost:3000',
   'http://localhost:3001',
-  'https://mycology.perenne.com/',
-  'https://myco-tex.web.app/'
+  'https://mycology.perenne.com',
+  'https://myco-tex.web.app'
 ]
 const corsOptions = {
   credentials: true, // This is important.
