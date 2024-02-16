4982f23903f441a33a949216a75e36b915b5e9f0
Ben Ballard <benb971@gmail.com>
2024-02-15 21:25:25 -0600

    eco config update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       host: '159.203.123.45',
       ref: 'origin/main',
       repo: 'https://github.com/BenjaminDBallard/myco-tex-backend.git',
-      path: '/home/benb/',
+      path: '/home/benb/mycotex-prod',
       node_args: '--experimental-specifier-resolution=node',
       env: {
         NODE_ENV: 'production'
